a98b68de9e4daabcd574a81291df272194f3dd77
georgiabasa <up1084016@ac.upatras.gr>
2024-05-19 15:29:34 +0300

    changed model

--- code/model/better-sqlite/model_lite.mjs
+++ code/model/better-sqlite/model_lite.mjs
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const db_name = path.join(__dirname, "../data", "library_db.db");
 
 
-
+//show to sugkekrimeno vivlio pros daneismo
 const showBook = (ISBN, callback) => {
     // ανάκτηση όλων των βιβλίων της βάσης δεδομένων
     const sql = "SELECT * FROM BOOK WHERE ISBN = ?";
@@ -26,7 +26,7 @@ const showBook = (ISBN, callback) => {
 });
 }
 
-
+//find ola ta vivlia vasei anazitisis
 const findBooks = (searchInput, callback) => {
    // ανάκτηση των βιβλίων αναλογα την αναζητηση
    const sql = `
@@ -55,80 +55,89 @@ const findBooks = (searchInput, callback) => {
 }); 
 }
 
-
-
-const insertUser = (firstName, lastName, email, phone, callback) => {
-    // εισαγωγή νέου χρήστη, και επιστροφή στο callback της νέας εγγραφής
-    const sql = "INSERT INTO USER(first_name, last_name, email, phoneNumber) VALUES (?,?,?,?);"
+//Επιστρέφει τον χρήστη με email 'email'
+const getUserByEmail = (email, callback) => {
+    const sql = "SELECT id FROM USER WHERE email = ? LIMIT 0, 1";
     const db = new sqlite3.Database(db_name);
-    db.run(sql, [firstName, lastName, email, phone], function (err, row){
+    db.all(sql, [email], (err, rows) => {
         if (err) {
             db.close();
-            callback(err, null)
+            callback(err, null);
         }
         db.close();
-        console.log('1 new user inserted', this.lastID);
-        //callback(null, [{"userID":this.lastID, "userName": userName}]); 
+        callback(null, rows[0]?.id); // επιστρέφει τον πρώτο χρήστη που βρέθηκε
     });
 }
 
-
-    
-    
-
-const findUser = (userName, password,callback) => {
-    const sql = "SELECT id FROM USER WHERE email = ? AND password = ? LIMIT 0, 1;"
-    console.log('new sql...', sql)
+//epistrefei to hashpass tou USER by email
+const getHashByEmail = (email, callback) => {
+    const sql = "SELECT hashpass FROM USER WHERE email = ? LIMIT 0, 1";
     const db = new sqlite3.Database(db_name);
-    db.all(sql, [userName, password], (err, row) => {
-        console.log("findUser")
-        if (err || row.length === 0) {
+    db.all(sql, [email], (err, rows) => {
+        if (err) {
             db.close();
-            return callback(null, null)
+            callback(err, null);
         }
-        else {
-            const userId = row[0].id
-            db.close();
-            callback(null, userId)
-        }
+        db.close();
+        callback(null, rows[0]?.hashpass); // επιστρέφει το hashpass του πρώτου χρήστη που βρέθηκε
     });
 }
 
-
-
-
-/**
- * Επιστρέφει τον χρήστη με όνομα 'username'
- */
-export let getUserByUsername = async (username) => {
-    const stmt = await sql.prepare("SELECT id, username, password FROM user WHERE username = ? LIMIT 0, 1");
-    try {
-        const user = await stmt.all(username);
-        return user[0];
-    } catch (err) {
-        throw err;
-    }
-}
-
-//Η συνάρτηση δημιουργεί έναν νέο χρήστη με password
-export let registerUser = async function (username, password) {
-    // ελέγχουμε αν υπάρχει χρήστης με αυτό το username
-    const userId = await getUserByUsername(username);
-    if (userId != undefined) {
-        return { message: "Υπάρχει ήδη χρήστης με αυτό το όνομα" };
-    } else {
-        try {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const stmt = await sql.prepare('INSERT INTO user VALUES (null, ?, ?)');
-            const info = await stmt.run(username, hashedPassword);
-            return info.lastID;
-        } catch (error) {
-            throw error;
+//eisagei neo xristi sti vasi
+const insertUser = (email, password, firstName, lastName, phone, callback) => {
+    // εισαγωγή νέου χρήστη, και επιστροφή στο callback της νέας εγγραφής
+    getUserByEmail(email, (err, userId) => {
+        if(err){
+            return callback(err,null);
         }
-    }
+        if(userId != undefined) {
+            const err1 = new Error("Email already used!");
+            return callback(err1, null);
+        }
+
+        bcrypt.hash(password,10, (err, hashedPassword) => {
+            if(err){
+                return callback(err, null);
+            }
+
+            const sql = "INSERT INTO USER(email, hashpass, first_name, last_name, phoneNumber) VALUES (?,?,?,?,?);"
+            const db = new sqlite3.Database(db_name);
+            db.run(sql, [email,hashedPassword, firstName, lastName, phone], function(err, row) {
+                if(err){
+                    db.close();
+                    return callback(err,null);
+                }
+                db.close();
+                console.log("1 new user inserted!", this.lastID);
+                callback(null, this.lastID);
+            });
+        });
+    });
 }
 
+//vriskei xristi apo ti vasi epistrefei id
+const findUser = (email, password,callback) => {
+    getHashByEmail(email, (err, hashpass) => {
+        if(err){
+            return callback(err,null);
+        }
+        if(hashpass === undefined) {
+            const err1 = new Error("This user does not exist!");
+            return callback(err1, null);
+        }
+        bcrypt.compare(password,hashpass, (err, result) => {
+            if(err){
+                return callback(err, null);
+            }
+            if(!result) {
+                const err2 = new Error("Password is incorrect!");
+                return callback(err2, null);
+            }
+            callback(null,true);
+        });
 
+    });
+};
 
-export {findBooks, showBook, insertUser, findUser};
+export {findBooks, showBook, getUserByEmail, getHashByEmail, insertUser, findUser};
 
